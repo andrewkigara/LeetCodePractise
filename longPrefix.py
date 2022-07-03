88b0f86f2d93dc6461972e4f151584c0ee3b8759
Andrew Kigara <andrewkigara@gmail.com>
2022-07-03 00:23:36 -0400

    new

---
+++ longPrefix.py
@@ -0,0 +1,18 @@
+def longestCommonPrefix(strs):
+    pref = []
+
+    for i in strs:
+        print(i)
+
+
+
+    if pref:
+        print(str(pref))
+    else:
+        print("")
+
+
+one1 = ["flower", "flow", "flight"]
+two2 = ["dog", "racecar", "car"]
+
+longestCommonPrefix(one1)
---
+++ romNum.py
@@ -0,0 +1,20 @@
+def romanToInt(s):
+    romNum = list(s)
+    print(romNum)
+    
+    
+
+
+
+
+
+
+
+
+
+
+one1 = "I"
+two2 = "II"
+oneNineNineFour = "MCMXCIV"
+
+romanToInt(oneNineNineFour)
